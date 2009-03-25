1091af210bcdf2761974350dae226c5e338e665e
andreo <andreo@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-03-25 14:01:17 +0000

    added GL_BLEND mode into base view by default added experimental special points and special segment to base view added experimental ggl support (https://svn.boost.org/svn/boost/sandbox/ggl) added ggl.cpp example

--- sandbox-branches/andreo/guigl/boost/guigl/view/base.hpp
+++ sandbox-branches/andreo/guigl/boost/guigl/view/base.hpp
@@ -18,6 +18,25 @@ namespace boost { namespace guigl {
 
 class window;
 
+struct LT {}; // left-top point
+struct LC {}; // left-center point
+struct LB {}; // left-bottom point
+struct CT {}; // center-top point
+struct CC {}; // center-center point
+struct CB {}; // center-bottom point
+struct RT {}; // right-top point
+struct RC {}; // right-center point
+struct RB {}; // right-bottom point
+
+struct VL {}; // vertical left segment
+struct VC {}; // vertical center segment
+struct VR {}; // vertical right segment
+struct HT {}; // horizontal left segment
+struct HC {}; // horizontal center segment
+struct HB {}; // horizontal right segment
+struct D1 {}; // LT-RB diagonal
+struct D2 {}; // LB-RT diagonal
+
 namespace view {
 
 class base
@@ -36,11 +55,56 @@ public:
 
     void set_size(const size_type &size)
     {   m_size = size; }
-    
+
+    template<class PositionTag>
+    position_type point() const;
+
+#define BOOST_GUIGL_SPECIAL_POINT_IMPL(point_tag_, val1_, val2_) \
+  template<>                                            \
+    inline position_type point<point_tag_>() const             \
+      { return position_type(val1_, val2_); }
+  
+    BOOST_GUIGL_SPECIAL_POINT_IMPL(LT, 0., 0.);
+    BOOST_GUIGL_SPECIAL_POINT_IMPL(LC, 0., size().y/2);
+    BOOST_GUIGL_SPECIAL_POINT_IMPL(LB, 0., size().y);
+    BOOST_GUIGL_SPECIAL_POINT_IMPL(CT, size().x/2, 0.);
+    BOOST_GUIGL_SPECIAL_POINT_IMPL(CC, size().x/2, size().y/2);
+    BOOST_GUIGL_SPECIAL_POINT_IMPL(CB, size().x/2, size().y);
+    BOOST_GUIGL_SPECIAL_POINT_IMPL(RT, size().x, 0.);
+    BOOST_GUIGL_SPECIAL_POINT_IMPL(RC, size().x, size().y/2);
+    BOOST_GUIGL_SPECIAL_POINT_IMPL(RB, size().x, size().y);
+
+    template<class SegmentTag>
+    inline segment_type segment() const;
+
+#define BOOST_GUIGL_SPECIAL_SEGMENT_IMPL(segment_tag_, point_tag1_, point_tag2_)    \
+  template<>                                                                        \
+  inline segment_type segment<segment_tag_>() const                                 \
+      {                                                                             \
+      return segment_type(point<point_tag1_>(), point<point_tag2_>());              \
+      }
+
+    BOOST_GUIGL_SPECIAL_SEGMENT_IMPL(VL, LT, LB);
+    BOOST_GUIGL_SPECIAL_SEGMENT_IMPL(VC, CT, CB);
+    BOOST_GUIGL_SPECIAL_SEGMENT_IMPL(VR, RT, RB);
+    BOOST_GUIGL_SPECIAL_SEGMENT_IMPL(HT, LT, RT);
+    BOOST_GUIGL_SPECIAL_SEGMENT_IMPL(HC, LC, RC);
+    BOOST_GUIGL_SPECIAL_SEGMENT_IMPL(HB, LB, RB);
+    BOOST_GUIGL_SPECIAL_SEGMENT_IMPL(D1, LT, RB);
+    BOOST_GUIGL_SPECIAL_SEGMENT_IMPL(D2, RT, LB);
+
 protected:
-    virtual void draw()=0;
-    void draw_prologue() {}
-    void draw_epilogue() {}
+    virtual void draw() = 0;
+    void draw_prologue()
+      {
+      glEnable(GL_BLEND);
+      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+      }
+
+    void draw_epilogue()
+      {
+      glDisable(GL_BLEND);
+      }
     
     virtual bool on_event(const event_type &e)
     {
@@ -57,7 +121,7 @@ protected:
 
 #define BOOST_GUIGL_WIDGET_DRAW             \
     friend class ::boost::guigl::access;    \
-    void BOOST_EXPORT_SYMBOLS draw();                            \
+    void BOOST_EXPORT_SYMBOLS draw();       \
     void draw_prologue();                   \
     void draw_epilogue();                   \
 
