650d94be935c69288690bf3c47c0dd3565899164
birbacher <birbacher@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-03-21 22:38:53 +0000

    Implemented property map for boost::function.

--- sandbox-branches/birbacher/propertymap-functormap/boost/property_map/functor_property_map.hpp
+++ sandbox-branches/birbacher/propertymap-functormap/boost/property_map/functor_property_map.hpp
@@ -11,6 +11,8 @@
 #define BOOST_FUNCTOR_PROPERTY_MAP_HPP
 
 #include <boost/property_map/property_map.hpp>
+#include <boost/function/function_fwd.hpp>
+#include <boost/type_traits/add_lvalue_reference.hpp>
 
 namespace boost {
 
@@ -33,6 +35,23 @@ namespace boost {
     typedef iterator_property_map<Functor, Arg> PA;
     return PA(f);
   }
+
+#if !defined(BOOST_FUNCTION_NO_FUNCTION_TYPE_SYNTAX)
+  // Preferred syntax
+  template <typename Key, typename Value>
+  struct property_traits<function<Value(Key)> > {
+    typedef Key key_type;
+    typedef Value value_type; 
+    typedef typename add_lvalue_reference<Value>::type reference;
+    typedef readable_property_map_tag category;
+  };
+  
+  template<typename Key, typename Value>
+  inline Value get(function<Value(Key)> const& f, Key const key)
+  {
+	return f(key);
+  }  
+#endif // have partial specialization
 
 } // namespace boost
 
