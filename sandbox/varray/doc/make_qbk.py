7df7783d7eaf9b935aaf5ab53b25d8c0cfc233f1
awulkiew <awulkiew@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-01-26 02:05:37 +0000

    Copying static_vector to varray

---
+++ sandbox/varray/doc/make_qbk.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# ===========================================================================
+#  Copyright (c) 2011-2012 Barend Gehrels, Amsterdam, the Netherlands.
+# 
+#  Use, modification and distribution is subject to the Boost Software License,
+#  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt)9
+# ============================================================================
+
+import os, sys
+
+cmd = "doxygen_xml2qbk"
+cmd = cmd + " --xml xml/%s.xml"
+cmd = cmd + " --start_include boost/"
+cmd = cmd + " --output_style alt"
+cmd = cmd + " > generated/%s.qbk"
+
+os.system("doxygen Doxyfile")
+
+os.system(cmd % ("classboost_1_1container_1_1static__vector", "static_vector"))
+os.system(cmd % ("group__static__vector__non__member", "static_vector_non_member"))
+os.system(cmd % ("structboost_1_1container_1_1strategy_1_1def", "strategy_def"))
+os.system(cmd % ("structboost_1_1container_1_1strategy_1_1allocator__adaptor", "strategy_allocator_adaptor"))
+
+os.system("b2") 
---
+++ sandbox/varray/example/static_vector_example.cpp
@@ -0,0 +1,40 @@
+/**
+ *  @file   static_vector_example.cpp
+ *  @date   Aug 14, 2011
+ *  @author Andrew Hundt <ATHundt@gmail.com>
+ *
+ *  (C) 2011-2012 Andrew Hundt <ATHundt@gmail.com>
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See
+ *  accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ *
+ *  @brief  static_vector_example.cpp demonstrates the use of boost::container::static_vector
+ *
+ */
+ 
+//[static_vector_example_cpp
+
+// static_vector_example.cpp
+
+#include <boost/container/static_vector.hpp>
+
+int main(int argc, char** argv){
+  
+  // static_vector of ints, fixed capacity: 3
+  boost::container::static_vector<int, 3> three; // size: 0
+
+  three.push_back(1);    // size: 1
+  three.push_back(2);    // size: 2
+  three.push_back(3);    // size: 3
+  
+  //three.reserve(4);    // no effect, fixed capacity: 3
+  //three.push_back(3);  // size: 4, behavior depends on strategy, assert by default
+  
+  three.pop_back();      // size: 2
+  three.shrink_to_fit(); // no effect, fixed capacity: 3
+  
+  return 0;
+}
+
+//]
