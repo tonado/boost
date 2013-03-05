58895a4d14926f7ca0f3b1c990105402d989eceb
lcaminiti <lcaminiti@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-03-05 21:46:15 +0000

    Updated copyright info.

--- sandbox/contract/libs/contract/example/contracts/body_natural.hpp
+++ sandbox/contract/libs/contract/example/contracts/body_natural.hpp
@@ -1,9 +1,9 @@
 
-// Copyright (C) 2008-2012 Lorenzo Caminiti
+// Copyright (C) 2008-2013 Lorenzo Caminiti
 // Distributed under the Boost Software License, Version 1.0
 // (see accompanying file LICENSE_1_0.txt or a copy at
 // http://www.boost.org/LICENSE_1_0.txt)
-// Home at http://sourceforge.net/projects/contractpp
+// Home at http://www.boost.org/doc/libs/release/libs/contract/doc/html/index.html
 
 #ifndef BODY_NATURAL_HPP_
 #define BODY_NATURAL_HPP_
